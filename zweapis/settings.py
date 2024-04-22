bbc503dad495497a8b8627abe3a8ca91f13a7fc3
Partson <mcwilton85@gmail.com>
2024-04-22 15:55:04 -0600

    Update settings.py

--- zweapis/settings.py
+++ zweapis/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xvu75kt34t=5d24_ayo$-$)m1zexfa(#=ibph2j*1zf5^aiaq7"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
